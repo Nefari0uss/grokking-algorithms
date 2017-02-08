34ab4d65f537bd2445a3b175110aba34f6ad6998
Nefari0uss <nnefari0uss@gmail.com>
2017-02-07 19:33:50 -0500

    renamed binary search

---
+++ binarySearch.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+
+def binary_search(list, item):
+    low = 0
+    high = len(list) - 1
+
+    while low <= high:
+        index = (low + high) // 2
+        num = list[index]
+        if num < item:
+            low = index + 1
+        elif num > item:
+            high = index - 1
+        elif num == item:
+            return index
+
+    return None
+
+
+test_list = [1, 3, 5, 7, 9]
+print("Original Array: ", test_list)
+print("Search for 3:   ", binary_search(test_list, 3))
+print("Search for -1:  ", binary_search(test_list, -1))
